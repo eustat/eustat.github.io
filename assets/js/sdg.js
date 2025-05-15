226d6672b8fc540acc9d9b2d00c84770e6ca1105
SendoaMasa <smasa@teknei.com>
2025-05-15 11:08:40 +0000

    Deploying to main from  @ 28a93c398ef1967fe4d7797464895a13114337d4 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3292,7 +3292,7 @@ var mapView = function () {
     $('.map').show();
     $('#map').sdgMap({
       indicatorId: indicatorId,
-      mapOptions: {"disaggregation_controls":true,"minZoom":5,"maxZoom":14,"tileURL":"https://www.geo.euskadi.eus/geoeuskadi/rest/services/U11/WMTS_GRIS/MapServer/WMTS/tile/1.0.0/U11_WMTS_GRIS/default/default028mm/{z}/{y}/{x}.png","tileOptions":{"id":"","accessToken":"","attribution":"<a href=\"https://www.geo.euskadi.eus/\" title=\"Eusko Jaurlaritza/Gobierno Vasco - geoEuskadi\">geoEuskadi</a>"},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":""}},
+      mapOptions: {"disaggregation_controls":false,"minZoom":5,"maxZoom":14,"tileURL":"https://www.geo.euskadi.eus/geoeuskadi/rest/services/U11/WMTS_GRIS/MapServer/WMTS/tile/1.0.0/U11_WMTS_GRIS/default/default028mm/{z}/{y}/{x}.png","tileOptions":{"id":"","accessToken":"","attribution":"<a href=\"https://www.geo.euskadi.eus/\" title=\"Eusko Jaurlaritza/Gobierno Vasco - geoEuskadi\">geoEuskadi</a>"},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":""}},
       mapLayers: [{"subfolder":"Territorio","label":"Territorio","min_zoom":5,"max_zoom":6,"staticBorders":true},{"subfolder":"Comarca","label":"Comarca","min_zoom":7,"max_zoom":8,"staticBorders":true},{"subfolder":"Municipio","label":"Municipio","min_zoom":9,"max_zoom":14,"staticBorders":true}],
       precision: precision,
       precisionItems: precisionItems,
@@ -6129,7 +6129,7 @@ $(function() {
             this.needsMapUpdate = false;
             this.seriesColumn = 'Series';
             this.unitsColumn = 'Units';
-            this.displayForm = true;
+            this.displayForm = false;
             this.updateDisaggregations(plugin.startValues);
         },
 
